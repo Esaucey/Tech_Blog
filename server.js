6f7424ca284009da7083fd51fcf4e580b1502919
Eric Shaw <ericshawmail@gmail.com>
2021-06-21 17:34:41 -0400

    post route fixed

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const app = express();
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
