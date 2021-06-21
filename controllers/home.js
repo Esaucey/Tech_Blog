b5dd7858baa5df323a3ac0c2c1eeb9cccfd1add5
Eric Shaw <ericshawmail@gmail.com>
2021-06-20 23:27:39 -0400

    updated home route

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,55 @@
 const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+          include: [User],
+        });
+    
+        const posts = postData.map((post) => post.get({ plain: true }));
+    
+        res.render('home', { posts });
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 router.get('/signup', (req, res) => {
     res.render('signup')
-})
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          User,
+          {
+            model: Comment,
+            include: [User],
+          },
+        ],
+      });
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('user-post', { post });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 module.exports = router;
